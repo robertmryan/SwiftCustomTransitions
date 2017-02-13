0ef5b2d9bcaaaf40032010495c4c9f77fd3f5606
Robert M. Ryan <robert.ryan@mindspring.com>
2017-02-13 10:44:44 -0800

    Initial Commit

---
+++ CustomTransitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTransitionDemo
+//
+//  Created by Robert Ryan on 2/13/17.
+//  Copyright © 2017 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
